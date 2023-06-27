ceff065b6ee98776bd15de26f4219f55fb17bf6f
Benjamin-Norton <bawnorton@me.com>
2023-06-26 22:57:59 -0400

    Add compat w/ no see no tick

---
+++ common/src/main/java/com/bawnorton/neruina/NeruinaMixinPlugin.java
@@ -0,0 +1,54 @@
+package com.bawnorton.neruina;
+
+import dev.architectury.injectables.annotations.ExpectPlatform;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Set;
+
+public class NeruinaMixinPlugin implements IMixinConfigPlugin {
+    @Override
+    public void onLoad(String mixinPackage) {
+
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        if(mixinClassName.equals("com.bawnorton.neruina.mixin.WorldMixin")) {
+            return !isModLoaded("noseenotick");
+        }
+        return true;
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+    }
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+    }
+
+    @ExpectPlatform
+    public static boolean isModLoaded(String modid) {
+        throw new AssertionError();
+    }
+}
---
+++ forge/src/main/java/com/bawnorton/neruina/mixin/forge/noseenotick/WorldMixin.java
@@ -0,0 +1,19 @@
+package com.bawnorton.neruina.mixin.forge.noseenotick;
+
+import com.bawnorton.neruina.Neruina;
+import net.minecraft.entity.Entity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(World.class)
+public abstract class WorldMixin {
+    @Inject(method = "shouldUpdatePostDeath", at = @At("HEAD"), cancellable = true)
+    public void shouldUpdatePostDeath(Entity entity, CallbackInfoReturnable<Boolean> cir) {
+        if(Neruina.isErrored(entity)) {
+            cir.setReturnValue(false);
+        }
+    }
+}
