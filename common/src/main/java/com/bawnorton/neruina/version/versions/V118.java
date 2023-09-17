35bde5ca33ecae45607dd3b76053e56d9d53ac0c
Benjamin-Norton <bawnorton@me.com>
2023-09-17 11:31:59 -0700

    reflection moment

--- common/src/main/java/com/bawnorton/neruina/version/versions/V118.java
+++ common/src/main/java/com/bawnorton/neruina/version/versions/V118.java
@@ -1,6 +1,5 @@
 package com.bawnorton.neruina.version.versions;
 
-import com.bawnorton.neruina.Neruina;
 import net.minecraft.text.Text;
 
 import java.lang.reflect.InvocationTargetException;
@@ -11,10 +10,8 @@ public abstract class V118 {
             Class<?> translatableTextClass = Class.forName("net.minecraft.class_2588");
             if(args.length == 0) return (Text) translatableTextClass.getConstructor(String.class).newInstance(key);
             else return (Text) translatableTextClass.getConstructor(String.class, Object[].class).newInstance(key, args);
-        } catch (ClassNotFoundException | InvocationTargetException | InstantiationException | IllegalAccessException |
-                 NoSuchMethodException e) {
-            Neruina.LOGGER.error("Failed to get translatable text for key " + key + "!", e);
-            return null;
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException("Failed to get translatable text of " + key + "!", e);
         }
     }
 
@@ -22,10 +19,8 @@ public abstract class V118 {
         try {
             Class<?> textClass = Class.forName("net.minecraft.class_2585");
             return (Text) textClass.getConstructor(String.class).newInstance(text);
-        } catch (ClassNotFoundException | InvocationTargetException | InstantiationException | IllegalAccessException |
-                 NoSuchMethodException e) {
-            Neruina.LOGGER.error("Failed to get text of " + text + "!", e);
-            return null;
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException("Failed to get text of " + text + "!", e);
         }
     }
 }
