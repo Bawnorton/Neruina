aacd641c083227c2eef56af9ca0a80e7f12664dd
Bawnorton <bawnorton@me.com>
2023-12-23 22:51:14 -0500

    Continue update to 1.20.4

--- NeruinaTestMod/forge/src/main/java/com/bawnorton/neruina_test/forge/NeruinaTestForge.java
+++ NeruinaTestMod/forge/src/main/java/com/bawnorton/neruina_test/forge/NeruinaTestForge.java
@@ -1,21 +1,11 @@
 package com.bawnorton.neruina_test.forge;
 
 import com.bawnorton.neruina_test.NeruinaTest;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(NeruinaTest.MOD_ID)
 public class NeruinaTestForge {
     public NeruinaTestForge() {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        modEventBus.addListener(NeruinaTestForge::init);
-        MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    private static void init(FMLCommonSetupEvent event) {
         NeruinaTest.init();
     }
 }
---
+++ NeruinaTestMod/neoforge/src/main/java/com/bawnorton/neruina_test/neoforge/NeruinaTestNeoForge.java
@@ -0,0 +1,11 @@
+package com.bawnorton.neruina_test.neoforge;
+
+import com.bawnorton.neruina_test.NeruinaTest;
+import net.neoforged.fml.common.Mod;
+
+@Mod(NeruinaTest.MOD_ID)
+public class NeruinaTestNeoForge {
+    public NeruinaTestNeoForge() {
+        NeruinaTest.init();
+    }
+}
